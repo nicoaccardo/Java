cc320c9554da19f5b90ac5eb8b5f96f268769c83
nicoaccardo <nicoloacc@gmail.com>
2023-11-28 16:02:06 +0100

    classe Rettangolo
    Stesso procedimento della classe Quadrato

---
+++ Esercitazioni/esercitazione2/es3/Rettangolo.java
@@ -0,0 +1,12 @@
+public class Rettangolo {
+    int base, altezza;
+
+    public int perimetro(){
+        return (base+altezza)*2;
+    }
+    
+    public int area(){
+        return base*altezza;
+    }
+
+}
