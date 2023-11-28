35463b40174981e2991c9fda2c7d4e7f1ddf7e9b
nicoaccardo <nicoloacc@gmail.com>
2023-11-28 13:38:24 +0100

    Operazioni Aritmetiche
    Esercizio 1 dell'esercitazione 2

---
+++ Esercitazioni/esercitazione2/es1/OperazioniAritmetiche.java
@@ -0,0 +1,27 @@
+public class OperazioniAritmetiche {
+    public static void main(String args[]) {
+
+        //Divisione
+        int a = 5, b = 3;
+        float r1 = a/b;
+
+        //Moltiplicazione
+        char c = 'a';
+        short s = 5000;
+        int r2 = c*s;
+
+        //Somma
+        int i = 6;
+        float f = 3.14F;
+        float r3 = i + f;
+
+        //Sottrazione
+        float r4 = r1-r2-r3;
+
+        System.out.println(r1);
+        System.out.println(r2);
+        System.out.println(r3);
+        System.out.println(r4);
+
+    }
+}
