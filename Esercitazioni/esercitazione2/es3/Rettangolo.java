4c91e9639fbd4136d3d7e5dc6de8c2b29e3f408f
nicoaccardo <nicoloacc@gmail.com>
2023-11-28 16:38:08 +0100

    Javadoc
    Aggiunta la documentazione dei file Rettangolo e Quadrato

--- Esercitazioni/esercitazione2/es3/Rettangolo.java
+++ Esercitazioni/esercitazione2/es3/Rettangolo.java
@@ -1,10 +1,13 @@
+/**Classe Rettangolo che calcola il perimetro e l'area */
 public class Rettangolo {
     int base, altezza;
 
+    /**Il perimetro del rettangolo è il doppio della somma della base e dell'altezza */
     public int perimetro(){
         return (base+altezza)*2;
     }
-    
+
+    /**L'area del rettangolo è il prodotto tra la base e l'altezza */
     public int area(){
         return base*altezza;
     }
