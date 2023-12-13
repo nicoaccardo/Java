50f284289cbde354fac9d12f523d2b7d73b8f1c5
nicoaccardo <nicoloacc@gmail.com>
2023-12-13 16:55:32 +0100

    Aggiornamento esercizio
    Sistemato il codice con l'aggiunta della randomizzazione e qualche altra piccolezza

--- PrincipaleV2.java
+++ PrincipaleV2.java
@@ -12,24 +12,35 @@ public class PrincipaleV2 {
         miaLista.add(p1);
         Persona p2 = new Persona("Alena","Curatolo",19);
         miaLista.add(p2);
+        Persona p3 = new Persona("Angelo","Accardo",52);
+        miaLista.add(p3);
+        Persona p4 = new Persona("Francesca","Di Giovanni",49);
+        miaLista.add(p4);
+        Persona p5 = new Persona("Giacomo","Accardo",17);
+        miaLista.add(p5);
 
         //verifica la dimensione della lista con il metodo size
-        System.out.println(miaLista.size());
+        System.out.println("La mia lista contiene " + miaLista.size() + " elementi");
 
-        //prendi l'i-esimo elemento dalla lista con il metodo get e stampalo
-        System.out.println(miaLista.get(1));
+        //prendi l'i-esimo elemento dalla lista con il metodo get e stampalo (ho aggiunto la randomizzazione)
+        System.out.println("\nPersona random: " + miaLista.get((int)(Math.random()*10 % miaLista.size())));
 
-        //elimina l'i-esimo elemento dalla lista con il metodo remove
-        miaLista.remove(1);
+        //elimina l'i-esimo elemento dalla lista con il metodo remove (ho aggiunto la randomizzazione)
+        if(miaLista.remove(miaLista.get((int)(Math.random()*10 % miaLista.size())))){
+            System.out.println("\nHo eliminato una persona random");
+        } else { System.out.println("\nErrore in fase di eliminazione"); }
 
         //stampa il contenuto della lista usando un ciclo for oppure un ciclo for migliorato(foreach)
+        System.out.println("\nNella lista sono presenti:");
         for(int i=0; i<miaLista.size(); i++){
             System.out.println(miaLista.get(i));
         }
 
         //Cancella l'intera lista con il metodo clear e verifica che sia effettivamente svuotata
         miaLista.clear();
-        System.out.println(miaLista.size());
+        if(miaLista.size()==0){
+            System.out.println("\nLista cancellata");
+        }
         
     }
 }
